701e2c9fe9d22afc73625577a2ef03371cb20b93
pooyadhgh <pooya.dhgh@gmail.com>
2021-10-13 12:49:14 +0330

    Add API and axios setting

---
+++ src/api/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+const instance = axios.create({
+  baseURL: `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMBDB_API_KEY}`,
+});
+
+const api = {
+  getData: params =>
+    instance({
+      params: {
+        ...params,
+      },
+    }),
+};
+
+export default api;
